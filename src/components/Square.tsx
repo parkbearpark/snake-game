9cc74269f78182ce59c1d9ff000ef7917c3f0fe2
yuma <yumai6205@gmail.com>
2023-11-14 14:12:46 +0900

    マス目コンポーネント 4状態ある

---
+++ src/components/Square.tsx
@@ -0,0 +1,31 @@
+import { type FC, useCallback } from "react"
+
+const STATUS_TABLE = {
+    'apple': 'red',
+    'body': 'green',
+    'head': 'blue',
+    'empty': 'black'
+} as const
+
+type SquareStatuses = keyof typeof STATUS_TABLE
+type SquareColors = typeof STATUS_TABLE[SquareStatuses]
+type Props = {
+  squareStatus: SquareStatuses
+}
+
+const Square: FC<Props> = ({ squareStatus }) => {
+    const squareColor = useCallback((s: SquareStatuses): SquareColors => {
+        return STATUS_TABLE[s]
+    }, [])
+
+    return (
+        <div
+            className="square"
+            style={{ backgroundColor: squareColor(squareStatus) }}
+        >
+
+        </div>
+    )
+}
+
+export { Square, type SquareStatuses, type SquareColors }
